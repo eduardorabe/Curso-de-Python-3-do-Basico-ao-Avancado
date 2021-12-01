643ea100736bd7edc5b8a22a294a6924f4ba493b
Eduardo R. Abe <eduardorabe.tech@gmail.com>
2021-12-01 10:26:08 -0300

    Primeiro commit

---
+++ Section 2 - Python Basico/aula1.py
@@ -0,0 +1,4 @@
+print('Aula sobre comentários em Python!')
+# Isso é um comentário!
+''''Isso também é um comentário!'''
+print("Blablaba")  # Comentário
---
+++ Section 2 - Python Basico/aula2.py
@@ -0,0 +1,6 @@
+print(123456)
+print('Eduardo', 'Abe', sep='-', end='fim')
+print('João', 'e', 'Maria', sep='-', end='')
+
+print('824', '176', '070', sep='.', end='-')
+print('18')
